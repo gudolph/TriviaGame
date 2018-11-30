8614134485b87e11ab6b8c5b5054aa1163e3a2c5
Andrew Waterhouse <gudolph@gmail.com>
2018-11-30 13:14:32 -0700

    before trying to make answer options clickable

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,57 @@
 window.onload = function () {
-    $("#start").click(timer.start);
+    $("#start").click(timer.start).click(quiz.start);
 };
 var intervalId;
 
 var clockRunning = false;
 
+var questions = [
+    "Which one of these is NOT one of Daenerys' dragons?",
+    "What is Littlefinger's sigil?",
+    "Who among these is a eunuch?",
+    "How many complete books are there in ASOIAF?",
+    "Who sent the assassin to try to kill Bran?",
+    "Which is NOT the name of a direwolf?",
+    "How many Stark children are greenseers",
+    "Which of these on Arya's kill list did she kill herself?",
+    "Who did Maggie the Frog predict would kill Cersei?",
+    "What is Young Griff's real name?",
+]
+
+var guesses = [
+    q1 = ["Viserion", "Rhaegon", "Drogon", "Rhaegal"],
+    q2 = ["Mockingbird", "A Tree", "Vulture", "Seven-Pointed Star"],
+    q3 = ["Illyrio", "Roose Bolton", "Varys", "Thoros"],
+    q4 = ["3", "4", "5", "6"],
+    q5 = ["Lysa Arryn", "Peter Baelish", "Joffrey Baratheon", "Jaime Lannister"],
+    q6 = ["Flower", "Lady", "Shaggydog", "Ghost"],
+    q7 = ["1", "2", "3", "All of them"],
+    q8 = ["Polliver", "Joffrey Baratheon", "Sir Ilyn Payne", "Sandor Clegane"],
+    q9 = ["Littlefinger", "The Valonquar", "The Sand Snakes", "The Three-Eyed Crow"],
+    q10 = ["Aegon", "Aemond", "Rhagar", "Rhanys"],
+]
+
+var correct = 0;
+var answers = ["Rhaegon", "Mockingbird", "Varys", "5", "Joffrey Baratheon", "Flower", "1", "Polliver", "The Valonquar", "Aegon"]
+var quiz = {
+    start: function () {
+        $("#question").text(questions[0]);
+        console.log(guesses[0]);
+        for (i = 0; i < 4; i++) {
+            $("#guesses").append("<li>" + guesses[0][i]);
+            var guess = (guesses[0][i]);
+        }
+        $.guess.click(function() {
+            var value = answers.inArray(guess)
+            console.log(value);
+            if (value == -1) {
+                correct++;
+            }
+        })
+    }
+}
 var timer = {
-    time: 65,
+    time: 90,
 
     start: function () {
         if (!clockRunning) {
@@ -16,7 +61,6 @@ var timer = {
     },
     count: function () {
         timer.time--;
-        console.log(timer.time)
         var displayTime = timer.countdown(timer.time);
         $("#timer").text(displayTime);
     },
@@ -24,20 +68,20 @@ var timer = {
 
         var minutes = Math.floor(t / 60);
         var seconds = t - (minutes * 60);
-    
+
         if (seconds < 10) {
-          seconds = "0" + seconds;
+            seconds = "0" + seconds;
         }
-    
+
         if (minutes === 0) {
-          minutes = "0";
+            minutes = "0";
         }
-    
+
         else if (minutes < 10) {
-          minutes = minutes;
+            minutes = minutes;
         }
-    
+
         return minutes + ":" + seconds;
-      }
+    }
 
 }
